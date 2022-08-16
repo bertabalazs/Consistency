824bb36cc21030bb608254196cc18bd8116bc7ce
Berta Balazs <berta.balazs@gmail.com>
2022-08-16 09:55:42 +0200

    modal without css

---
+++ 2.-Modal/script.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('.modal-btn')
+const modalOverlay = document.querySelector('.modal-overlay')
+const closeBtn = document.querySelector('.close-btn')
+
+btn.addEventListener('click', function(){
+    modalOverlay.classList.add('open-modal')
+})
+
+closeBtn.addEventListener('click', function(){
+    modalOverlay.classList.remove('open-modal')
+})
+
